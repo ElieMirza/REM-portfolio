dd45856be4975632031272a8e4162a4d27fb4668
Elie Mirza <83608815+ElieMirza@users.noreply.github.com>
2025-07-30 09:46:58 +0300

    Update App component to set basename for BrowserRouter to support GitHub Pages deployment

--- client/App.tsx
+++ client/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/REM-portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
